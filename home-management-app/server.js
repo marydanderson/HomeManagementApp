f6e25bef3f406a8cd3bb6c84c3116f3cde68b251
marydanderson <maryd.anderson3s@gmail.com>
2022-07-14 12:26:01 -0400

    Deployment

--- home-management-app/server.js
+++ home-management-app/server.js
@@ -9,8 +9,10 @@ app.use(express.static("./dist/home-management-app"));
 
 // Route incoming server request to the correct files
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/home-management-app" })
+  res.sendFile("index.html", { root: "dist/home-management-app/" })
 });
 
 //Sstart the app on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
